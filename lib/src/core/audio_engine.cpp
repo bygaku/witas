a017119501c4e189967eeb7db284b2c27a928f91
intwisty <intmainwist@gmail.com>
2026-05-30 14:23:12 +0900

    Add: Skeleton classes

--- lib/src/core/audio_engine.cpp
+++ lib/src/core/audio_engine.cpp
@@ -2,3 +2,62 @@
  * Created by intwi on 2026/05/30.
  * Copyright (c) 2026 All rights reserved.
  */
+
+#include "audio_engine.h"
+
+#include <cstdio>
+
+#include "sound_handle.h"
+#include "miniaudio.h"
+
+namespace wit {
+
+/** AudioEngine::Impl */
+class AudioEngine::Impl {
+public:
+	Impl() = default;
+	~Impl() = default;
+
+	/** TODO */
+};
+
+/** AudioEngine */
+AudioEngine::AudioEngine()
+	: impl_(std::make_unique<Impl>()) {
+}
+
+AudioEngine::~AudioEngine() {
+	if (true /** TODO */ ) {
+		Stop();	///< If AudioEngine::Stop() has already been called in the user code, skip this step
+	}
+}
+
+bool AudioEngine::Start() {
+	std::fprintf(stderr, "[wit] AudioEngine::Start() not yet implemented (stage 1)\n");
+	return false;
+}
+
+void AudioEngine::Stop() {
+}
+
+std::optional<SoundHandle> AudioEngine::LoadSound(const char* path) {
+	(void)path;
+	std::fprintf(stderr, "[wit] AudioEngine::LoadSound() not yet implemented (stage 1)\n");
+	return std::nullopt;
+}
+
+SoundHandle AudioEngine::Register(AudioData&& audio) {
+	(void)audio;
+	std::fprintf(stderr, "[wit] AudioEngine::Register() not yet implemented (stage 1)\n");
+	return SoundHandle{};
+}
+
+void AudioEngine::Play(SoundHandle handle) {
+	(void)handle;
+}
+
+void AudioEngine::Unload(SoundHandle handle) {
+	(void)handle;
+}
+
+}
---
+++ lib/src/core/audio_engine.h
@@ -0,0 +1,78 @@
+/**
+ * Created by intwi on 2026/05/29.
+ * Copyright (c) 2026 All rights reserved.
+ */
+
+#ifndef WIT_AUDIO_ENGINE_H
+#define WIT_AUDIO_ENGINE_H
+
+#include <memory>
+#include <optional>
+
+#include "audio_data.h"
+
+namespace wit {
+
+class SoundHandle;
+
+/**
+ * @class AudioEngine
+ * @brief
+ *
+ */
+class AudioEngine {
+public:
+	/**
+	 */
+	AudioEngine();
+
+#pragma region /** Disable copy and move this class */
+	AudioEngine(const AudioEngine&)            = delete;
+	AudioEngine& operator=(const AudioEngine&) = delete;
+	AudioEngine(AudioEngine&&)                 = delete;
+	AudioEngine& operator=(AudioEngine&&)      = delete;
+#pragma endregion
+
+	/**
+	 */
+	~AudioEngine();
+
+	/**
+	 */
+	bool Start();
+
+	/**
+	 */
+	void Stop();
+
+	/**
+	 */
+	std::optional<SoundHandle> LoadSound(const char* file_path);
+
+	/**
+	 */
+	SoundHandle Register(AudioData&& audio);
+
+	/**
+	 * Play the sound from 'SoundHandle'.
+	 * @param handle
+	 */
+	void Play(SoundHandle handle);
+
+	/**
+	 * Release 'SoundHandle' from memory.
+	 * @param handle
+	 * @note A sound handle that is currently playing will be unloaded after it stops.
+	 */
+	void Unload(SoundHandle handle);
+
+private:
+	class Impl;	///< Implemented in audio_engine.cpp
+
+	std::unique_ptr<Impl> impl_;	///< Add implementation not exposed to users
+
+};
+
+}
+
+#endif //WIT_AUDIO_ENGINE_H

151a5be15d61ba7c3c51bd4cea901fee03a107e4
intwisty <intmainwist@gmail.com>
2026-06-10 23:40:31 +0900

    Add: public header file

---
+++ lib/src/public/wit.h
@@ -0,0 +1,25 @@
+/**
+ * Created by wistd on 2026/06/10.
+ * Copyright (c) 2026 All rights reserved.
+ */
+
+#ifndef WIT_WIT_H
+#define WIT_WIT_H
+
+
+struct WitSoundPlayerTag;
+
+typedef WitSoundPlayerTag* WitSoundPlayerHn;
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+    /**
+     * TODO: function here.
+     */
+#ifdef __cplusplus
+    }
+#endif
+
+#endif //WIT_WIT_H
